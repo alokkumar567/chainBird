064ff188a51762e80d8d3d7396695d515ee48998
alokkumar567 <133910999+alokkumar567@users.noreply.github.com>
2023-11-25 23:38:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# chainBird
+This is the repository to contain Dapp Twitter Clone using React and Solidity
---
+++ client/src/config.js
@@ -0,0 +1,2 @@
+// export const TwitterContractAddress = "0x5326AE9D1268F1F9F0267C8dB7C86D7d58c69c52"
+export const TwitterContractAddress = "0xa6E84F0c488994Cf172019A9024bc507283874F5"
